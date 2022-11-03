c8df373c1d7d9782b4617adf8c14ae21e7953dd3
Perry Jorss <pjorss123@gmail.com>
2022-11-03 18:25:02 -0400

    added get dealership route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json
+  set :default_content_type, 'application/json'
+
+  # Add your routes here
+  get "/dealerships" do
+    dealerships = Dealership.all
+    dealerships.to_json
+  end
+
 
 end
